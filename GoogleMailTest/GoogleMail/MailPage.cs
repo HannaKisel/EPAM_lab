6a3ef0eb4dc30bbae0b7b5b193f30f5de82949c1
Hanna Maroz <Hanna_Maroz2@epam.com>
2018-07-06 17:05:40 +0300

    Add file gitignore and add GoogleMailTest

---
+++ GoogleMailTest/GoogleMail/MailPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace GoogleMail
+{
+  public class MailPage
+  {
+    private const string RECIPIENTADRESS="hanna_maroz2@epam.com";
+    private readonly By RecipientArea = By.XPath("//div[text()='Получатели']");
+
+    private readonly By WriteButton = By.XPath("//div[text()='НАПИСАТЬ']");
+    private IWebDriver Driver;
+
+    public MailPage(IWebDriver webDriver)
+    {
+      Driver = webDriver;
+    }
+
+    public void WriteNewMessage()
+    {
+      WaitForElement(WriteButton);
+      Driver.FindElement(WriteButton).Click();
+    }
+
+    public void WaitForElement(By locator)
+    {
+      new WebDriverWait(Driver, TimeSpan.FromSeconds(10)).Until(ExpectedConditions.ElementIsVisible(locator));
+    }
+  }
+}
---
+++ GoogleMailTest/GoogleMail/SettingsPage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace GoogleMail
+{
+  public class SettingsPage
+  {
+    private readonly By GoogleAppsButton = By.XPath("//div[@id='gbwa']");
+    private readonly By MailButton = By.XPath("//span[text()='Почта']");
+
+    private IWebDriver Driver;
+
+    public SettingsPage(IWebDriver webDriver)
+    {
+      Driver = webDriver;
+    }
+
+    public MailPage GoToMailPage()
+    {
+      WaitForElement(GoogleAppsButton);
+      Driver.FindElement(GoogleAppsButton).Click();
+      WaitForElement(MailButton);
+      Driver.FindElement(MailButton).Click();
+      return new MailPage(Driver);
+    }
+
+    public void WaitForElement(By locator)
+    {
+      new WebDriverWait(Driver, TimeSpan.FromSeconds(10)).Until(ExpectedConditions.ElementIsVisible(locator));
+    }
+  }
+}
